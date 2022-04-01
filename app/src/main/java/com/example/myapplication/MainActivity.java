c870f03470aa1d4c40ab17be6a322e3feae2d231
matth <matthieu.sahinidis@epfedu.fr>
2022-04-01 10:24:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class MainActivity<image1> extends AppCompatActivity {
+    private ImageView imageView;
+    private Button button1;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageView test = findViewById(R.id.imageView3);
+//
+//        Bitmap image1 = BitmapFactory.decodeFile("scarlett.png");
+//        test.setImageBitmap(image1);
+
+        this.imageView = (ImageView) this.findViewById(R.id.imageView);
+
+        this.button1 = (Button) this.findViewById(R.id.button);
+        this.button2 = (Button) this.findViewById(R.id.button2);
+
+        this.button1.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showImage1();
+            }
+        });
+
+        this.button2.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showImage2();
+            }
+        });
+    }
+
+
+    private void showImage1() {
+        this.imageView.setImageResource(R.drawable.robertdeniro);
+    }
+
+    private void showImage2() {
+        this.imageView.setImageResource(R.drawable.scarlett);
+    }
+    }
+
+
---
+++ app/src/main/java/com/example/myapplication/image.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import static android.graphics.BitmapFactory.decodeFile;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ImageView;
+
+public class image {
+    String categorie[] = {"Acteurs", "Acteur X", "Peufien", "Youtubers/streamer","TDB", "Sportif","Chanteur", "Perso de film"};
+
+    public void test(){
+
+//        File path = new File("C:\\Users\\matth\\Documents\\Cours\\S3\\Programmation\\Projet_quiestce_final\\app\\src\\main\\res\\drawable-v24\\categorie test");
+//            ArrayList<Image> cate1 = new ArrayList<Image>();
+//            for (File file : path.listFiles()) {
+//            try {
+//                BufferedImage img = ImageIO.read(file);
+//                if (img != null)
+//                    cate1.add(img);
+//                else
+//                    cate1.add(img);
+//
+//            } catch (IOException e) {
+//                continue;
+//            }
+//        }
+//            for (int i=0; i>cate1.size(); i++){
+//                if (cate1!=null){
+//                    System.out.print("0 ");
+//                }
+//            }
+
+
+//        Bitmap image = decode   File("C:\\Users\\matth\\Documents\\Cours\\S3\\Programmation\\MyApplication\\image dossier");
+//        List<Bitmap> images = new ArrayList<Bitmap>();
+//        images.add(image);
+//        for (int i=0; i<images.size() ; i++){
+//            if(images[i] != null){
+//                System.out.print("0 ");
+//            }
+//        }
+    }
+}
