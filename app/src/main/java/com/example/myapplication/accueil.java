57ae2b777bf1b6183169f921407dd49b30604131
matth <matthieu.sahinidis@epfedu.fr>
2022-04-01 11:16:14 +0200

    rajout de l'ecran d'accueil

--- app/src/main/java/com/example/myapplication/accueil.java
+++ app/src/main/java/com/example/myapplication/accueil.java
@@ -3,12 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class accueil extends AppCompatActivity {
+    private Button creerpartie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
+        this.creerpartie = (Button) this.findViewById(R.id.creerpartie);
+
+
     }
 }
