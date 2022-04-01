5ab83add89ea7791569d76fb94003a5e74d7d45f
matth <matthieu.sahinidis@epfedu.fr>
2022-04-01 11:12:55 +0200

    rajout de l'ecran d'accueil

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.ImageView;
 
 public class MainActivity<image1> extends AppCompatActivity {
     private ImageView imageView;
-    private Button button1;
+    private Button button;
     private Button button2;
 
     @Override
@@ -20,10 +20,10 @@ public class MainActivity<image1> extends AppCompatActivity {
 
         this.imageView = (ImageView) this.findViewById(R.id.imageView1);
 
-        this.button1 = (Button) this.findViewById(R.id.button);
+        this.button = (Button) this.findViewById(R.id.button);
 
 
-        this.button1.setOnClickListener(new Button.OnClickListener() {
+        this.button.setOnClickListener(new Button.OnClickListener() {
 
             @Override
             public void onClick(View v) {
