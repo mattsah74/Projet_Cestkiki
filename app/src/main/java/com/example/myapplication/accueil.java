5a9e15281ebee5da4b8448cc5375c73952b9e94c
matth <matthieu.sahinidis@epfedu.fr>
2022-04-03 17:57:44 +0200

    rajout de la fenetre d'accueil et je commence a comprendre comment marche cette application de ses morts

--- app/src/main/java/com/example/myapplication/accueil.java
+++ app/src/main/java/com/example/myapplication/accueil.java
@@ -6,16 +6,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class accueil extends AppCompatActivity {
-    private Button creerpartie;
+public class accueil<image1> extends AppCompatActivity {
+    private Button cat1;
+    private Button cat2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int [] cateselec = new int[2];
+        cateselec[0] = 0;
+        cateselec[1] = 0;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-        this.creerpartie = (Button) this.findViewById(R.id.creerpartie);
+        this.cat1 = (Button) this.findViewById(R.id.cat1);
+        this.cat2 = (Button) this.findViewById(R.id.cat2);
+
+        this.cat1.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                rajoutcate(cateselec, 1);
+        }});
 
 
+        this.cat2.setOnClickListener(new Button.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                rajoutcate(cateselec, 2);
+            }});
+
+
+    }
+    private void rajoutcate(int[] tab, int cat){
+        if (tab[0] != 0){
+            if (tab[0]<cat){
+                tab[1] = cat;
+            }
+            else{
+                int inter = tab[0];
+                tab[0] = cat;
+                tab[1] = inter;
+            }
+        }
+        else{
+            tab[1] = cat;
+        }
     }
 }
